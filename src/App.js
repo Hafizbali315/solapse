2820f852aeb4e6036870db3f97524fd93667fc80
hafizbali315 <hafizbali315@gmail.com>
2022-04-05 03:03:12 +0500

    Items Details Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import PriceDetails from './components/PriceDetails'
 import Footer from './components/Footer'
+import ItemsDetails from './components/ItemsDetails'
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<ItemsDetails />
 			<PriceDetails />
 			<Footer />
 		</div>
---
+++ src/components/ItemsDetails.js
@@ -0,0 +1,30 @@
+const ItemsDetails = () => {
+	return (
+		<div className="items_details">
+			<div className="two_items">
+				<div className="item">
+					<div className="name">T-Shirts</div>
+					<div className="quantity">100</div>
+				</div>
+
+				<div className="item">
+					<div className="name">Caps</div>
+					<div className="quantity">30</div>
+				</div>
+			</div>
+			<div className="two_items">
+				<div className="item">
+					<div className="name">Solape NFTs</div>
+					<div className="quantity">20</div>
+				</div>
+
+				<div className="item">
+					<div className="name">SOLAPE Tokens</div>
+					<div className="quantity">20</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ItemsDetails
